5896b4bd1a2beb93b4063c1a88b9888baaa7a5c8
Nishan Chettri <35814272+nishanchettri@users.noreply.github.com>
2020-01-23 15:37:47 +0530

    Add files via upload

---
+++ 7SegmentDisplay/7SegmentDisplay.ino
@@ -0,0 +1,14 @@
+#include <SegmentDisplay.h>
+SegmentDisplay segmentDisplay(2, 3, 4, 5, 6, 7, 8, 9);
+void setup() {
+  // put your setup code here, to run once:
+
+
+}
+
+void loop() {
+  int val=analogRead(A0);
+  val=map(val,0,1023,0,10);
+  // put your main code here, to run repeatedly:
+segmentDisplay.displayHex(val, false);
+}
